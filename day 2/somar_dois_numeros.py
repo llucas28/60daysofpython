b370a663dea6f4d0a9cde583e4cb1d99ba6da4a8
llucas28 <lucashenrique_182@hotmail.com>
2025-10-15 17:22:19 -0300

    adição de numeros

---
+++ day 2/somar_dois_numeros.py
@@ -0,0 +1,8 @@
+numero1 = input("Digite um numero:")
+numero2 = input("Digite outro numero:")
+
+print(type(numero1))
+
+soma_dois_numeros = int(numero1) + int(numero2)
+
+print(f"Essa é a soma de dois numeros em python {soma_dois_numeros}")
