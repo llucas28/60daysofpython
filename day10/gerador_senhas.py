0982c7ebcda174c8354eee77adf2302d99784d64
llucas28 <lucashenrique_182@hotmail.com>
2025-10-15 22:21:36 -0300

    add dia 10 gerador de senhas

---
+++ day10/gerador_senhas.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def gerador_de_senhas(tamanho):
+    comprimento = tamanho
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''
+
+    while len(senha) < comprimento:
+        senha += random.choice(caracteres)
+
+    print(f"Sua senha ficou assim: {senha}")
+gerador_de_senhas(14)
